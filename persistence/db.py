d8808e2555d7a02537abd19ff7b2f3070bda2935
Alexis Bejarano <aletzisbejaranov@hotmail.com>
2024-12-03 20:57:31 -0700

    Se creo ramita

--- persistence/db.py
+++ persistence/db.py
@@ -5,6 +5,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host='localhost',
         user= 'root',
-        password= 'aliz',
-        database= 'cheems'
+        password= '15546BEJ',
+        database= 'cheems',
+        port= '53088'
     )
